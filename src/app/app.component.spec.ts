517ede39b5324c96bc72374141511c0bbb52108e
JimmyRuedaG <jimmy960711@hotmail.com>
2021-09-10 18:37:11 -0500

    'arreglos'

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,11 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'lineaIII'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('lineaIII');
-  });
+ 
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgModel } from '@angular/forms';
+
+export interface PeriodicElement {
+  nombre: string;
+  posicion: number;
+  Cedula: number;
+  NumeroCelular: string;
+}
+
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { posicion: 1, nombre: '', Cedula: 1.0079, NumeroCelular: 'H' },
+  { posicion: 2, nombre: 'Helium', Cedula: 4.0026, NumeroCelular: 'He' },
+  { posicion: 3, nombre: 'Lithium', Cedula: 6.941, NumeroCelular: 'Li' },
+  { posicion: 4, nombre: 'Beryllium', Cedula: 9.0122, NumeroCelular: 'Be' },
+  { posicion: 5, nombre: 'Boron', Cedula: 10.811, NumeroCelular: 'B' },
+  { posicion: 6, nombre: 'Carbon', Cedula: 12.0107, NumeroCelular: 'C' },
+  { posicion: 7, nombre: 'Nitrogen', Cedula: 14.0067, NumeroCelular: 'N' },
+  { posicion: 8, nombre: 'Oxygen', Cedula: 15.9994, NumeroCelular: 'O' },
+  { posicion: 9, nombre: 'Fluorine', Cedula: 18.9984, NumeroCelular: 'F' },
+  { posicion: 10, nombre: 'Neon', Cedula: 20.1797, NumeroCelular: 'Ne' },
+];
 
 @Component({
   selector: 'app-root',
@@ -6,6 +28,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
+  displayedColumns: string[] = ['posicion', 'nombre', 'Cedula', 'NumeroCelular'];
+  dataSource = ELEMENT_DATA;
 
 
 }
