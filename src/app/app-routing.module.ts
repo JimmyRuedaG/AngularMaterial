c1965df21d7c482196fa4f337293dfa6179af364
JimmyRuedaG <jimmy960711@hotmail.com>
2021-09-14 11:10:33 -0500

    routerlink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegistroComponent } from './pages/registro/registro.component';
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
-  { path: 'registro', component: RegistroComponent}
+  { path: 'registro', component: RegistroComponent},
+  { path: 'inicio', component: AppComponent }
+
 ];
 
 @NgModule({
