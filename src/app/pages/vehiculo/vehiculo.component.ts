688b5f5feb79e084c8a80b3e32a41c59437c208d
JimmyRuedaG <jimmy960711@hotmail.com>
2021-10-03 18:19:45 -0500

    service de vehiculo y model

--- src/app/pages/vehiculo/vehiculo.component.ts
+++ src/app/pages/vehiculo/vehiculo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Vehiculo } from 'src/app/_model/Vehiculo';
+import { VehiculoService } from 'src/app/_service/vehiculo.service';
 
 @Component({
   selector: 'app-vehiculo',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VehiculoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private vehiculoService: VehiculoService) { }
 
   ngOnInit(): void {
+
   }
 
 }
